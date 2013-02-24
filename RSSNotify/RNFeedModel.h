8284391370a7305a8fba16c5ea6a66fc830ca938
Brian Rogers <infinitetape@gmail.com>
2013-02-24 11:34:43 -0500

    Added asi-http-request library

---
+++ RSSNotify/RNFeedModel.h
@@ -0,0 +1,13 @@
+//
+//  RNFeedModel.h
+//  RSSNotify
+//
+//  Created by Brian Rogers on 2/24/13.
+//  Copyright (c) 2013 Brian Rogers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNFeedModel : NSObject
+
+@end
---
+++ RSSNotify/RNFeedModel.m
@@ -0,0 +1,13 @@
+//
+//  RNFeedModel.m
+//  RSSNotify
+//
+//  Created by Brian Rogers on 2/24/13.
+//  Copyright (c) 2013 Brian Rogers. All rights reserved.
+//
+
+#import "RNFeedModel.h"
+
+@implementation RNFeedModel
+
+@end

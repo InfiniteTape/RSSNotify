4d0d13a1d23368591c47c206298a3462bb92c4e9
Brian Rogers <infinitetape@gmail.com>
2013-03-25 22:15:52 -0400

    First shot at a status item

---
+++ RSSNotify/RNMenulet.h
@@ -0,0 +1,15 @@
+//
+//  RNMenulet.h
+//  RSSNotify
+//
+//  Created by Brian Rogers on 3/25/13.
+//  Copyright (c) 2013 Brian Rogers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RNMenulet : NSObject {
+    NSStatusItem *statusItem;
+}
+
+@end
---
+++ RSSNotify/RNMenulet.m
@@ -0,0 +1,24 @@
+//
+//  RNMenulet.m
+//  RSSNotify
+//
+//  Created by Brian Rogers on 3/25/13.
+//  Copyright (c) 2013 Brian Rogers. All rights reserved.
+//
+
+#import "RNMenulet.h"
+
+@implementation RNMenulet
+
+- (void)awakeFromNib {
+    statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    [statusItem setHighlightMode:YES];
+    [statusItem setTitle:@"0.0.0.0"];
+    [statusItem setEnabled:YES];
+    [statusItem setToolTip:@"RNMenulet"];
+    
+    //[statusItem setAction:@selector(updateIPAddress:)];
+    //[statusItem setTarget:self];
+}
+
+@end
